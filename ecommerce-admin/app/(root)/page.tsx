3f5b72e19e5c830804d6588e445a412e0c6321ea
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-01 10:50:06 +0000

    modal created

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal isOpen onClose={() => {}} title="Test" description="Test desc">
+        Children
+      </Modal>
     </div>
   );
 };
