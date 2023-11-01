00c02c47a43d96c0c7aaa0de4adaba4681855211
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-01 10:24:08 +0000

    clerk auth setup

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button size="default" variant="destructive">
-        Click me
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+
+export default SetupPage;
