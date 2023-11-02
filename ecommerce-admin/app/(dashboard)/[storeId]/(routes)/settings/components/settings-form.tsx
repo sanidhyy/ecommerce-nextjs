5ef9b732af9d6a16bd8bb72f2482d7c0d82f663b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-02 17:31:56 +0000

    store delete functionality implemented

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,6 +22,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 type SettingsFormProps = {
   initialData: Store;
@@ -52,7 +53,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
       router.refresh();
 
-      toast.success("Store updated.");
+      toast.success("Store updated successfully.");
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
@@ -60,8 +61,31 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     }
   };
 
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+
+      router.refresh();
+      router.push("/");
+
+      toast.success("Store deleted successfully.");
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
 
