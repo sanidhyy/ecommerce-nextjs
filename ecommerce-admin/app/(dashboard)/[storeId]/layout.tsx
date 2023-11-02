e209b9a1b150c6926a80a54d8eca88fc5ae91249
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-02 16:07:12 +0000

    navbar components added

--- ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,8 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import Navbar from "@/components/navbar";
+
 export default async function DashboardLayout({
   children,
   params,
@@ -25,7 +27,7 @@ export default async function DashboardLayout({
   return (
     <>
       <div>
-        Welcome to Dashboard
+        <Navbar />
         {children}
       </div>
     </>
