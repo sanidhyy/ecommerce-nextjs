3aa5144a0685e77485ed808005d5f2de204ae64f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-03 11:10:15 +0000

    image upload functionality added

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,7 +1,22 @@
-const BillboardPage = async () => {
+import prismadb from "@/lib/prismadb";
+import BillboardForm from "./components/billboard-form";
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
   return (
-    <div>
-      <h1>BillboardPage</h1>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+      </div>
     </div>
   );
 };
