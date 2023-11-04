135ce14743066a572a8fa1af97b00951e88b7ca1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-04 13:45:45 +0000

    cell actions completed

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -13,12 +13,12 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { BillBoardColumn } from "./columns";
+import { SizeColumn } from "./columns";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 type CellActionProps = {
-  data: BillBoardColumn;
+  data: SizeColumn;
 };
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -29,21 +29,19 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard Id copied to the clipboard.");
+    toast.success("Size Id copied to the clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
 
       router.refresh();
 
-      toast.success("Billboard deleted successfully.");
+      toast.success("Size deleted successfully.");
     } catch (error) {
-      toast.error(
-        "Make sure you removed all categories using this billboard first."
-      );
+      toast.error("Make sure you removed all products using this size first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -75,9 +73,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
