12de6817fb6d1419cc1a1dd47cd8025dd2beed9f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-04 14:36:47 +0000

    color cell actions completed

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -13,12 +13,12 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { SizeColumn } from "./columns";
+import { ColorColumn } from "./columns";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 type CellActionProps = {
-  data: SizeColumn;
+  data: ColorColumn;
 };
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -29,19 +29,19 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Size Id copied to the clipboard.");
+    toast.success("Color Id copied to the clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
 
       router.refresh();
 
-      toast.success("Size deleted successfully.");
+      toast.success("Color deleted successfully.");
     } catch (error) {
-      toast.error("Make sure you removed all products using this size first.");
+      toast.error("Make sure you removed all products using this color first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -73,7 +73,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+            onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
