a41f429aa00b5a0dcdf5c85b3dac858fafacc56f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-05 09:40:47 +0000

    layout files created

---
+++ ecommerce-store/components/navbar.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/ui/container";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <Container>
+        <h1>Navbar</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
