6268b9f5de3971db9c265fc54887c12a743ba2af
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-05 10:38:35 +0000

    fetched category data from api

--- ecommerce-store/components/navbar.tsx
+++ ecommerce-store/components/navbar.tsx
@@ -1,10 +1,22 @@
-import Container from "@/components/ui/container";
+import Link from "next/link";
 
-const Navbar = () => {
+import Container from "@/components/ui/container";
+import MainNav from "@/components/main-nav";
+import getCategory from "@/actions/get-category";
+
+const Navbar = async () => {
+  const categories = await getCategory();
+
   return (
     <div className="border-b">
       <Container>
-        <h1>Navbar</h1>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+
+          <MainNav data={categories} />
+        </div>
       </Container>
     </div>
   );
