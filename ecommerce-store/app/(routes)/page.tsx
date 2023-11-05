fcb6803bcc732f847476100a7fefc87e9e4fa097
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-05 11:21:11 +0000

    fetched billboards from api

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("f0adcda2-7404-4208-9497-6b473987f1bd");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
