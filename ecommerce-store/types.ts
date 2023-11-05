44ca02578e41a86205ba51d8a00441267e94927e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-05 12:01:06 +0000

    new types added

--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -9,3 +9,31 @@ export type Category = {
   name: string;
   billboard: Billboard;
 };
+
+export type Product = {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[];
+};
+
+export type Image = {
+  id: string;
+  url: string;
+};
+
+export type Size = {
+  id: string;
+  name: string;
+  value: string;
+};
+
+export type Color = {
+  id: string;
+  name: string;
+  value: string;
+};
