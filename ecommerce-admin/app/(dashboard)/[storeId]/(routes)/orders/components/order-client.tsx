1f14f948a801b7f6724f05eeae57fcb9da6665ea
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-05 09:05:02 +0000

    orders front-end implemented

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/components/order-client.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { OrderColumn, columns } from "./columns";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+type OrderClientProps = {
+  data: OrderColumn[];
+};
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+
+      <DataTable columns={columns} data={data} searchKey="label" />
+    </>
+  );
+};
