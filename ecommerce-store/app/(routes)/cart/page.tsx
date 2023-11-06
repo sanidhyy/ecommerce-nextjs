5d2d76f228d00e56e0f32274d6bb60023c8cc55a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 13:07:38 +0000

    shopping cart ui implemented

---
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Container from "@/components/ui/container";
+import CartItem from "./components/cart-item";
+import useCart from "@/hooks/use-cart";
+
+const CartPage = () => {
+  const cart = useCart();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CartPage;
--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import useCart from "@/hooks/use-cart";
-import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ShoppingBag } from "lucide-react";
+
+import useCart from "@/hooks/use-cart";
+import Button from "@/components/ui/button";
 
 const NavbarActions = () => {
+  const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -18,7 +21,10 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
 
         <span className="ml-2 text-sm font-medium text-white">
