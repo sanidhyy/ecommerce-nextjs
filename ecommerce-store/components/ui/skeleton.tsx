198456dfcfc8b78d078d75e2a46e8b844f50583f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 16:30:17 +0000

    skeleton loading style fixes

--- ecommerce-store/components/ui/skeleton.tsx
+++ ecommerce-store/components/ui/skeleton.tsx
@@ -1,15 +1,15 @@
 import { cn } from "@/lib/utils";
 
-function Skeleton({
+const Skeleton = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-neutral-200", className)}
       {...props}
     />
   );
-}
+};
 
-export { Skeleton };
+export default Skeleton;
