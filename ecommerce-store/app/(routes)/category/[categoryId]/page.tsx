17091e934b4dd3ed2b32136a324a0e5e4f7ca37a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 11:48:47 +0000

    color filters added

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -2,10 +2,12 @@ import getCategory from "@/actions/get-category";
 import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
-import Billboard from "@/components/billboard";
 
-import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
 import Filter from "./components/filter";
+import Container from "@/components/ui/container";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 type CategoryPageProps = {
   params: {
@@ -41,6 +43,16 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             {/* add mobile filters */}
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((product) => (
+                  <ProductCard key={product.id} data={product} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
