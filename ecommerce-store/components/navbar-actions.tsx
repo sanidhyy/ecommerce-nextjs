5b3d973df03955447ac3a78eb1e5a5df2f41d237
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 12:49:14 +0000

    cart items hook created

--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -11,6 +12,8 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const cart = useCart();
+
   if (!isMounted) return null;
 
   return (
@@ -18,7 +21,9 @@ const NavbarActions = () => {
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
 
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
