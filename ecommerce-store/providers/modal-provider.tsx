09e3c0611dcf3ef2f54bfb05586608f6ef5c2c8b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 12:38:41 +0000

    modal provider created

---
+++ ecommerce-store/providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PreviewModal from "@/components/preview-modal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // prevent hydration errors
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+};
+
+export default ModalProvider;
