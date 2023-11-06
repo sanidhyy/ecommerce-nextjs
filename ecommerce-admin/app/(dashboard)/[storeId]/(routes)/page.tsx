30902e1769fe6bc2cb97ab93944f358514024ae9
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 14:45:19 +0000

    dashboard counts added

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -6,6 +6,8 @@ import { Separator } from "@/components/ui/separator";
 
 import { formatter } from "@/lib/utils";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
 
 type DashboardPageProps = {
   params: { storeId: string };
@@ -13,8 +15,8 @@ type DashboardPageProps = {
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const totalRevenue = await getTotalRevenue(params.storeId);
-  const salesCount = () => {};
-  const stockCount = () => {};
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
 
   return (
     <div className="flex-col">
@@ -43,7 +45,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-3xl font-bold">+25</div>
+              <div className="text-3xl font-bold">+{salesCount}</div>
             </CardContent>
           </Card>
 
@@ -55,7 +57,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <Package className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-3xl font-bold">12</div>
+              <div className="text-3xl font-bold">{stockCount}</div>
             </CardContent>
           </Card>
         </div>
