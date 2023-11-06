2d38561ee59b9d4c2ef2bf50cf97caf490f23662
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 12:48:49 +0000

    react hot toast provider created

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from "next/font/google";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={urbanist.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
