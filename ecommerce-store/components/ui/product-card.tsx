7caefa02d5070cf4ab0d04ec3db41c575b299fbe
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 12:39:01 +0000

    individual product preview modal added

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -1,21 +1,31 @@
 "use client";
 
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 import { Expand, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
+import usePreviewModal from "@/hooks/use-preview-modal";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
-import { useRouter } from "next/navigation";
 
 type ProductCardProps = {
   data: Product;
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
+
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -34,7 +44,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
