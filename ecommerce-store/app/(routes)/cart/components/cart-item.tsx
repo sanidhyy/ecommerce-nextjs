66ead6cbe94be499c75674b06cf395b929d11d48
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 13:09:15 +0000

    remove from cart functionality added

--- ecommerce-store/app/(routes)/cart/components/cart-item.tsx
+++ ecommerce-store/app/(routes)/cart/components/cart-item.tsx
@@ -14,6 +14,12 @@ type CartItemProps = {
 };
 
 const CartItem: React.FC<CartItemProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onRemove = () => {
+    cart.removeItem(data.id);
+  };
+
   return (
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
@@ -27,7 +33,7 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
 
       <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
         <div className="absolute z-10 right-0 top-0">
-          <IconButton onClick={() => {}} icon={<X size={15} />} />
+          <IconButton onClick={onRemove} icon={<X size={15} />} />
         </div>
 
         <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
