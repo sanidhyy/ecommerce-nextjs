ff4d7ead86223e14e7eba93dabade318f0648ac0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-06 12:01:35 +0000

    mobile filters created

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -4,10 +4,11 @@ import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
 
 import Billboard from "@/components/billboard";
-import Filter from "./components/filter";
 import Container from "@/components/ui/container";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
+import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 type CategoryPageProps = {
   params: {
@@ -40,7 +41,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* add mobile filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
