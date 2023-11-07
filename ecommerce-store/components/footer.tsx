e01673c5eb0ec7a96fec4fe105f1c4e46eea61b3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-07 13:00:47 +0000

    [store] dynamic name added

--- ecommerce-store/components/footer.tsx
+++ ecommerce-store/components/footer.tsx
@@ -1,9 +1,13 @@
+import { SITE_NAME } from "@/constants";
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; {new Date().getFullYear()} Store | All rights reserved.
+          &copy; {new Date().getFullYear()}-
+          {(new Date().getFullYear() % 100) + 1} {SITE_NAME} | All rights
+          reserved.
         </p>
       </div>
     </footer>
--- ecommerce-store/components/navbar.tsx
+++ ecommerce-store/components/navbar.tsx
@@ -5,6 +5,7 @@ import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 import { ShoppingCart } from "lucide-react";
+import { SITE_NAME } from "@/constants";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -15,7 +16,7 @@ const Navbar = async () => {
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <ShoppingCart size={20} />
-            <p className="font-bold text-xl">STORE</p>
+            <p className="font-bold text-xl uppercase">{SITE_NAME}</p>
           </Link>
 
           <MainNav data={categories} />
