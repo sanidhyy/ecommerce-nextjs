7323832febe90f7191c4f771e5531c18dfd24295
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-07 11:32:00 +0000

    nav icon added

--- ecommerce-store/components/navbar.tsx
+++ ecommerce-store/components/navbar.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
+import { ShoppingCart } from "lucide-react";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -13,6 +14,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <ShoppingCart size={20} />
             <p className="font-bold text-xl">STORE</p>
           </Link>
 
