7d125a00884157bb39d534eac7bdcf8f52587b88
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-08 14:30:16 +0000

    [admin] source code link added

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,10 +1,14 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
 import ThemeToggle from "@/components/theme-toggle";
+import { Github } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { EXTRA_LINKS } from "@/constants";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -23,6 +27,16 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <Link
+            href={EXTRA_LINKS.source_code}
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Github"
+          >
+            <Button variant="outline">
+              <Github className="h-5 w-5" />
+            </Button>
+          </Link>
           <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
